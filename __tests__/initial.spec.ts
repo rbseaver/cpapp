64792997739420e3d45fceacd252a2aae4da1e67
Rob Seaver <rob.seaver@gmail.com>
2025-03-29 16:58:25 -0500

    test: remove dummy test since we know this works

--- __tests__/initial.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('when checking for proper jest functioning', () => {
-  it('1 should be 1 and pass', () => {
-    expect(1).toEqual(1);
-  });
-});
