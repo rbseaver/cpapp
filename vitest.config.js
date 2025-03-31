acebadec6c8e24e32906f51b6d2e236c050db3b6
Rob Seaver <rob.seaver@gmail.com>
2025-03-31 18:14:15 -0500

    chore: small changes to test coverage config

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,23 @@
 /// <reference types="vitest/config" />
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    dir: './__tests__'
+    dir: './__tests__',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        '**/__tests__/**',
+        '**/__mocks__/**',
+        '**/node_modules/**',
+        '**/.github/**',
+        '*.config.js'
+      ],
+      provider: 'v8',
+      all: true
+    },
   }
 });
