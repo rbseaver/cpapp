baed4cde21cd928c9f01c3d7bdd78ca883ca7601
Rob Seaver <rob.seaver@gmail.com>
2025-03-31 16:21:22 -0500

    test: basic version services tests, filesystem mock

--- __tests__/services/version.service.spec.ts
+++ __tests__/services/version.service.spec.ts
@@ -1,16 +1,61 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 
 import VersionService from '../../src/services/version.service';
+import { vol } from 'memfs';
+
+vi.mock('node:fs')
+vi.mock('node:fs/promises')
 
 describe('when calling the version service', () => {
-  it('should return the version number', () => {
-    // Arrange
-    const versionService = new VersionService();
+  describe('and all is well', () => {
+    beforeEach(() => {
+      vol.fromJSON({
+        'package.json': JSON.stringify({
+          name: 'test',
+          version: '1.0.0'
+        }),
+      });
+    });
 
-    // Act
-    const version = versionService.getVersion();
+    afterEach(() => {
+      vi.restoreAllMocks();
+      vol.reset();
+    });
 
-    // Assert
-    expect(version).toBe('1.0.0');
+
+    it('should return the version number', async () => {
+      // Arrange
+      const versionService = new VersionService();
+
+      // Act
+      const version = await versionService.getVersion();
+
+      // Assert
+      expect(version).toBe('1.0.0');
+    });
+  });
+
+  describe('and the file is unreadable', () => {
+    beforeEach(() => {
+      vol.fromJSON({
+        'package-jkasdlf.json': JSON.stringify(''),
+      });
+    });
+
+    afterEach(() => {
+      vi.restoreAllMocks();
+      vol.reset();
+    });
+
+    it('should return the default version number', async () => {
+      // Arrange
+      const versionService = new VersionService();
+
+      // Act
+      const version = await versionService.getVersion();
+
+      // Assert
+      expect(version).toBe('0.0.0');
+    });
   });
 });
--- src/services/version.service.ts
+++ src/services/version.service.ts
@@ -1,7 +1,27 @@
+import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
+import path from 'path';
+
 class VersionService {
-  getVersion = (): string => {
-    return '1.0.0';
+  private cachedVersion: string = '';
+
+  getVersion = async (): Promise<string> => {
+    if (this.cachedVersion) {
+      return this.cachedVersion;
+    }
+
+    try {
+      const packagePath = path.resolve('.', 'package.json');
+      const data = await readFile(packagePath, 'utf8');
+      const packageJson = JSON.parse(data);
+      this.cachedVersion = packageJson.version;
+      return this.cachedVersion!;
+    } catch (error) {
+      console.error('Error reading version:', error);
+      return '0.0.0';
+    }
   }
 }
+
 
 export default VersionService;
