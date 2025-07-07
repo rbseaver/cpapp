aaaa6c56191af05fa0cd7fe632b67ef643f24845
Rob Seaver <rob.seaver@gmail.com>
2025-07-07 18:44:35 -0500

    refactor(version): use constant for version endpoint
    Introduce a static readonly string `Endpoint` for the version API call, replacing the hardcoded string in the `GetAsync` method. Change `await using` to `using` for the `factory` variable to modify its disposal behavior.

--- CPApp.Tests.Integration/WhenCallingVersionController.cs
+++ CPApp.Tests.Integration/WhenCallingVersionController.cs
@@ -9,19 +9,22 @@ namespace CPApp.Tests.Integration
         private static readonly Regex SemVerRegex = new("^\\d+\\.\\d+\\.\\d+(-[A-Za-z0-9\\-.]+)?(\\+[A-Za-z0-9\\-.]+)?$");
 #pragma warning restore SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
 
+        private static readonly string Endpoint = "/version";
+
         [TestMethod]
         public async Task ShouldRetrieveFullVersionString()
         {
             // Arrange  
-            await using var factory = new IntegrationTestWebApplicationFactory<Program>();
+            using var factory = new IntegrationTestWebApplicationFactory<Program>();
             using var client = factory.CreateClient();
 
             // Act  
-            var response = await client.GetAsync("/version");
+            var response = await client.GetAsync(Endpoint);
+            var responseString = await response.Content.ReadAsStringAsync();
 
             // Assert  
             response.EnsureSuccessStatusCode();
-            var responseString = await response.Content.ReadAsStringAsync();
+
 
             StringAssert.Matches(responseString, SemVerRegex);
         }
